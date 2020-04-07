94c34a795ee4502c81df1b30de02456d1b5963f0
saul.maos@gmail.com <ZarahyTrezbg>
2020-04-06 21:16:18 -0500

    First commit - added firebase

---
+++ app/src/main/java/com/mal/saul/firebasedemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mal.saul.firebasedemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
